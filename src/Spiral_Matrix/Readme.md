a4870129f3d9d3287b701cd8be9c81af4ae2bb3d
shuyu liu <liushuyu6666@gmail.com>
2023-08-02 23:12:58 -0400

    update Spiral_Matrix

--- src/Spiral_Matrix/Readme.md
+++ src/Spiral_Matrix/Readme.md
@@ -1,9 +1,10 @@
 # Note of Spiral Matrix
-## Matrix
+## Solution
 1. Establish boundaries in all four directions - left, right, up, and down.
 2. "squashing" the boundaries.
 
-**Caveat**:
-1. In each component of the `while` loop, it is necessary to verify `left <= right && up <= down`, as these variables 
-are modified within the main loop. These four components must adhere to the same boundary conditions as the outer loop.
+## Caveat
+1. redundant conditional checking: The condition is redundantly checked within each part of the loop's body. This approach is adopted due to the following reasons:
+   1. **Sequential Execution**: Each part is executed within the loop in sequence.
+   2. **Dynamic Variable Updates**: Since conditional variables will be updated within each part, it is necessary to reevaluate the condition before proceeding to the next part.
 ![Squash_the_boundaries.png](Squash_the_boundaries.png)
--- src/Spiral_Matrix/Solution.java
+++ src/Spiral_Matrix/Solution.java
@@ -1,49 +1,84 @@
 package Spiral_Matrix;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Solution {
-    public int[] spiralOrder(int[][] matrix) {
-        if(matrix.length == 0) return new int[0];
-        int left = 0, right = matrix[0].length - 1, up = 0, down = matrix.length - 1;
-        int m = matrix.length, n = matrix[0].length;
-        int[] ans = new int[m * n];
-        int i = 0;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
 
-        while(left <= right && up <= down) {
-            int x = up, y = left;
-            // up margin
-            while(y <= right && left <= right && up <= down) {
-                ans[i] = matrix[x][y];
-                i++;
-                y++;
-            }
-            up++;
+        int m = matrix.length; // 3
+        int n = matrix[0].length; // 4
 
-            // right margin
-            x = up; y = right;
-            while(x <= down && left <= right && up <= down) {
-                ans[i] = matrix[x][y];
-                i++;
-                x++;
-            }
-            right--;
+        int leftTop_x = 0, leftTop_y = 0, rightBottom_x = m - 1, rightBottom_y = n - 1;
 
-            // down margin
-            x = down; y = right;
-            while(y >= left && left <= right && up <= down) {
-                ans[i] = matrix[x][y];
-                i++;
-                y--;
+        while(leftTop_x <= rightBottom_x && leftTop_y <= rightBottom_y) {
+            if(leftTop_x <= rightBottom_x && leftTop_y <= rightBottom_y) {
+                // leftTop: [1, 1]; rightBottom: [1, 2];
+                List<Integer> newList = listTop(matrix, leftTop_x, leftTop_y, rightBottom_x, rightBottom_y);
+                ans = Stream.concat(ans.stream(), newList.stream()).toList();
+                leftTop_x++; // 2
             }
-            down--;
+            if(leftTop_x <= rightBottom_x && leftTop_y <= rightBottom_y) {
+                // leftTop: [1, 0]; rightBottom: [2, 3];
+                List<Integer> newList = listRight(matrix, leftTop_x, leftTop_y, rightBottom_x, rightBottom_y);
+                ans = Stream.concat(ans.stream(), newList.stream()).toList();
+                rightBottom_y--; // 2
+            }
+            if(leftTop_x <= rightBottom_x && leftTop_y <= rightBottom_y) {
+                // leftTop: [1, 0]; rightBottom: [2, 2];
+                List<Integer> newList = listBottom(matrix, leftTop_x, leftTop_y, rightBottom_x, rightBottom_y);
+                ans = Stream.concat(ans.stream(), newList.stream()).toList();
+                rightBottom_x--; // 1
+            }
+            if(leftTop_x <= rightBottom_x && leftTop_y <= rightBottom_y) {
+                // leftTop: [1, 0]; rightBottom: [1, 2];
+                List<Integer> newList = listLeft(matrix, leftTop_x, leftTop_y, rightBottom_x, rightBottom_y);
+                ans = Stream.concat(ans.stream(), newList.stream()).toList();
+                leftTop_y++; // 1
+            }
 
-            // left margin
-            x = down; y = left;
-            while(x >= up && left <= right && up <= down) {
-                ans[i] = matrix[x][y];
-                i++;
-                x--;
-            }
-            left++;
+        }
+
+        return ans;
+    }
+
+    public List<Integer> listTop(int[][] matrix, int leftTop_x, int leftTop_y, int rightBottom_x, int rightBottom_y) {
+        List<Integer> ans = new ArrayList<>();
+        // [1, 2]
+        for(int i = leftTop_y; i <= rightBottom_y; i++) {
+            ans.add(matrix[leftTop_x][i]); // (1, i)
+        }
+
+        return ans;
+    }
+
+    public List<Integer> listBottom(int[][] matrix, int leftTop_x, int leftTop_y, int rightBottom_x, int rightBottom_y) {
+        List<Integer> ans = new ArrayList<>();
+        // [2, 0]
+        for(int i = rightBottom_y; i >= leftTop_y; i--) {
+            ans.add(matrix[rightBottom_x][i]); // 2
+        }
+
+        return ans;
+    }
+
+    public List<Integer> listRight(int[][] matrix, int leftTop_x, int leftTop_y, int rightBottom_x, int rightBottom_y) {
+        List<Integer> ans = new ArrayList<>();
+        // [1, 2]
+        for(int i = leftTop_x; i <= rightBottom_x; i++) {
+            ans.add(matrix[i][rightBottom_y]); // (i, 3)
+        }
+
+        return ans;
+    }
+
+    public List<Integer> listLeft(int[][] matrix, int leftTop_x, int leftTop_y, int rightBottom_x, int rightBottom_y) {
+        List<Integer> ans = new ArrayList<>();
+        // [1, 1]
+        for(int i = rightBottom_x; i >= leftTop_x; i--) {
+            ans.add(matrix[i][leftTop_y]); // (i, 0)
         }
 
         return ans;
