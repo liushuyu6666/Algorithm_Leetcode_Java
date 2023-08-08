7040ffc6e4695317ed4af0c126a6c9530951ccad
shuyu liu <liushuyu6666@gmail.com>
2023-08-08 15:07:05 -0400

    update the comments here

--- src/Matrix_Block_Sum/Readme.md
+++ src/Matrix_Block_Sum/Readme.md
@@ -20,3 +20,7 @@ Recall the equation:
 $$mat(x, y) -> prefix(x + 1, y + 1)$$
 
 We need to write the general expression: `ans[i][j] = prefix[bx + 1][by + 1] - prefix[ax + 1][by + 1] - prefix[bx + 1][ay + 1] + prefix[ax + 1][ay + 1];` first and then consider the boundary conditions and set appropriate limitations.
+
+Additional information is elucidated within the code's accompanying comments. A pertinent concern arises when considering the variables `ax`, `ay`, `bx`, and `by` within the context of the `mat` coordinates. It is imperative that these variables adhere to the following constraints: $ax \geq 0$, $ay \geq 0$, $bx \le m$, and $by \le n$. However, it is noteworthy that the generation of `ax`, `ay`, `bx`, and `by` primarily serves the purpose of calculating the prefix sum. Consequently, it is advisable to utilize these variables within the `prefix` coordinates. Actually, in this question we should put everything under the `prefix` coordinates.
+
+![ax_bx_ay_by.png](ax_bx_ay_by.png)
--- src/Matrix_Block_Sum/Solution.java
+++ src/Matrix_Block_Sum/Solution.java
@@ -3,6 +3,7 @@ package Matrix_Block_Sum;
 public class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int m = mat.length, n = mat[0].length;
+
         int[][] prefix = new int[m + 1][n + 1];
 
         for(int i = 1; i < m + 1; i++) {
@@ -13,11 +14,24 @@ public class Solution {
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
+                /**
+                 * (i, j) is under the mat coordinates, regardless of the boundary, we have
+                 * int ax = i - k - 1, bx = i + k;
+                 * int ay = j - k - 1, by = j + k;
+                 * In general, regardless of the boundary,
+                 * we need ans(i, j) = Pf(bx, by) - Pf(ax, by) - Pf(bx, ay) + Pf(ax, ay), where Pf(x, y) is the prefix sum of the (x, y) in the mat.
+                 * We already know Pf(x, y) = prefix(x + 1, y + 1);
+                 * Then we have ans(i, j) = prefix(bx + 1, by + 1) - prefix(ax + 1, by + 1) - prefix(bx + 1, ay + 1) + prefix(ax + 1, ay + 1)
+                 *   ax + 1 >= 0      =>    ax >= -1;
+                 *   bx + 1 < m + 1   =>    bx < m;
+                 *   ay + 1 >= 0      =>    ay >= -1;
+                 *   by + 1 < n + 1   =>    by < n;
+                 */
                 int ax = Math.max(-1, i - k - 1);
                 int bx = Math.min(m - 1, i + k);
                 int ay = Math.max(-1, j - k - 1);
                 int by = Math.min(n - 1, j + k);
-                mat[i][j] = prefix[bx + 1][by + 1] - prefix[ax + 1][by + 1] - prefix[bx + 1][ay + 1] + prefix[ax + 1][ay + 1];
+                mat[i][j] = prefix[bx + 1][ by + 1] - prefix[ax + 1][ by + 1] - prefix[bx + 1][ ay + 1] + prefix[ax + 1][ ay + 1];
             }
         }
 
